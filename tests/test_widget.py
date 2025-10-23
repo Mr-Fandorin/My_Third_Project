4d48b01264b65a36ff60464ebf8eb16b93760855
Sergey Mosykin <smosyakin@yandex.ru>
2025-10-23 13:15:05 +0300

    fixing test_widget.py for test_get_date

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -3,6 +3,7 @@ import pytest
 
 from src.widget import mask_account_card, get_date
 
+
 @pytest.mark.parametrize('value, expected', [
     ('Maestro 1596837868705199', 'Maestro 1596 83** **** 5199'),
     ('', 'нет данных'),
@@ -20,3 +21,13 @@ from src.widget import mask_account_card, get_date
 def test_mask_account_card(value, expected):
     assert mask_account_card(value) == expected
 
+@pytest.mark.parametrize('value, expected', [
+    ('2024-03-11T02:26:18.671407', '11.03.2024'),
+    ('', 'нет данных'),
+    ('2024:03:11T02:26:18', '11.03.2024'),
+    ('2024/03/11T02/26:18', '11.03.2024'),
+    ('2024-03-1', 'недостаточно данных')
+])
+
+def  test_get_date(value, expected):
+    assert  get_date(value) == expected
