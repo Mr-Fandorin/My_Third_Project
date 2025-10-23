59922d415e482ea9775abb3eb2312e575f31b6e1
Sergey Mosykin <smosyakin@yandex.ru>
2025-10-23 11:28:57 +0300

    add processing.py

---
+++ src/processing.py
@@ -0,0 +1,14 @@
+def filter_by_state(list_of_user_dates: list[dict], state="EXECUTED") -> list[dict]:
+    """Функция, фильтрующая словари по нужному ключу"""
+    new_list_of_users = []
+    for item in list_of_user_dates:
+        for value in item.values():
+            if value == state:
+                new_list_of_users.append(item)
+    return new_list_of_users
+
+
+def sort_by_date(list_of_user_dates: list[dict], sorting_type=True) -> list[dict]:
+    """Функция, сортирующая словари по нужному ключу"""
+    sorted_list_of_users = sorted(list_of_user_dates, key=lambda dict_user: dict_user["date"], reverse=sorting_type)
+    return sorted_list_of_users
