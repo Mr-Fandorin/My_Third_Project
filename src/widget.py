d1da790cd959fd9ea024526de3b40968b9781978
Sergey Mosykin <smosyakin@yandex.ru>
2025-10-23 20:44:53 +0300

    fixing widget.py

--- src/widget.py
+++ src/widget.py
@@ -1,6 +1,7 @@
-import masks
-
 import re
+
+from . import masks
+
 
 def mask_account_card(card_account_num: str) -> str:
     """Функция маскирующая номер счета или карты"""
@@ -9,18 +10,18 @@ def mask_account_card(card_account_num: str) -> str:
             mask_account_num = masks.get_mask_account(int(card_account_num[-20:]))
             return f"{card_account_num[:-20]}{mask_account_num}"
         else:
-            return 'неверный номер счета'
+            return "неверный номер счета"
     elif re.search("[a-zA-Z]", card_account_num):
-        list_card_num = card_account_num.split(' ')
+        list_card_num = card_account_num.split(" ")
         if len(list_card_num[-1]) == 16:
             mask_card_num = masks.get_mask_card_number(int(card_account_num[-17:]))
             return f"{card_account_num[:-17]} {mask_card_num}"
         else:
-            return 'неверный номер карты'
+            return "неверный номер карты"
     elif len(card_account_num) == 0:
-        return 'нет данных'
+        return "нет данных"
     else:
-        return 'недостаточно данных'
+        return "недостаточно данных"
 
 
 def get_date(full_date: str) -> str:
