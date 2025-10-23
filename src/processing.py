103087c89b3fc76f4f9aea92c5ed963e3ca95b12
Sergey Mosykin <smosyakin@yandex.ru>
2025-10-23 20:44:28 +0300

    fixing masks.py, processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,7 @@
-def filter_by_state(list_of_user_dates: list[dict], state="EXECUTED") -> list[dict]:
+from typing import Union
+
+
+def filter_by_state(list_of_user_dates: list[dict], state="EXECUTED") -> Union[str, list[dict]]:
     """Функция, фильтрующая словари по нужному ключу"""
     new_list_of_users = []
     for item in list_of_user_dates:
@@ -6,7 +9,7 @@ def filter_by_state(list_of_user_dates: list[dict], state="EXECUTED") -> list[di
             if value == state:
                 new_list_of_users.append(item)
     if new_list_of_users == []:
-        return 'нет данных'
+        return "нет данных"
     else:
         return new_list_of_users
 
@@ -15,4 +18,3 @@ def sort_by_date(list_of_user_dates: list[dict], sorting_type=True) -> list[dict
     """Функция, сортирующая словари по нужному ключу"""
     sorted_list_of_users = sorted(list_of_user_dates, key=lambda dict_user: dict_user["date"], reverse=sorting_type)
     return sorted_list_of_users
-
