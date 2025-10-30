a029d39c70503e5c450fca8f14590f661f15c03a
Sergey Mosykin <smosyakin@yandex.ru>
2025-10-30 22:05:30 +0300

    add func card_number_generator

--- src/generators.py
+++ src/generators.py
@@ -13,3 +13,12 @@ def transaction_descriptions(transactions):
         i += 1
 
 
+def card_number_generator(a, b):
+    x = a
+    while x <= b:
+        str_card_num = "0000000000000000"
+        lenght_num = len(str(x))
+        new_num = str_card_num[:16 - lenght_num]
+        full_num = new_num + str(x)
+        yield f"{full_num[:4]} {full_num[4:8]} {full_num[8:12]} {full_num[12:]}"
+        x += 1
