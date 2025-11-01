01dd0272696026364d65b37236d8c149bc0b9a6d
Sergey Mosykin <smosyakin@yandex.ru>
2025-11-01 11:43:09 +0300

    fixing

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,7 +1,7 @@
 import pytest
 
 from src.generators import card_number_generator, filter_by_currency, transaction_descriptions
-from tests.conftest import transactions
+from conftest import transactions
 
 
 @pytest.mark.parametrize(
