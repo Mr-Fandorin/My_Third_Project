0cf4c73c70af9a77b3b9d7fe3f2809a05813d6f3
Sergey Mosykin <smosyakin@yandex.ru>
2025-11-02 15:39:25 +0300

    fixing func card_number_generator (typing, delete "x += 1")

--- src/generators.py
+++ src/generators.py
@@ -1,7 +1,8 @@
 from typing import Iterator
+from typing import Union
 
 
-def filter_by_currency(transactions: list[dict], currency: str) -> Iterator:
+def filter_by_currency(transactions: list[dict], currency: str) -> Iterator[Union[dict, str]]:
     "Генератор функции сортировки базы данных по заданной валюте"
     filtered_people = list(filter(lambda x: x["operationAmount"]["currency"]["code"] == currency, transactions))
     if not filtered_people:
@@ -16,7 +17,7 @@ def filter_by_currency(transactions: list[dict], currency: str) -> Iterator:
                 break
 
 
-def transaction_descriptions(transactions: list[dict]) -> Iterator:
+def transaction_descriptions(transactions: list[dict]) -> Iterator[str]:
     "Генератор функции вывода описаний транзакций"
     if not transactions:
         yield "Нет данных"
@@ -30,13 +31,12 @@ def transaction_descriptions(transactions: list[dict]) -> Iterator:
                 break
 
 
-def card_number_generator(start: int, stop: int) -> Iterator:
+def card_number_generator(start: int, stop: int) -> Iterator[str]:
     "Генератор функции генерирования номеров карт в заданном диапазоне"
     for x in range(start, stop + 1):
         lenght_num = len(str(x))
         if lenght_num <= 16:
             str_card_num = f"{x:016d}"
             yield f"{str_card_num[:4]} {str_card_num[4:8]} {str_card_num[8:12]} {str_card_num[12:]}"
-            x += 1
         else:
             yield "неверный номер карты"
