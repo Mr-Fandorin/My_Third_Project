c8bac47730cbcd21f04bdbd4d8ea157e6ff44d33
Sergey Mosykin <smosyakin@yandex.ru>
2025-11-06 23:39:44 +0300

    fixing decorators.py add test_decorators.py

--- src/decorators.py
+++ src/decorators.py
@@ -1,23 +1,28 @@
 from time import time
 
-def log(filename):
+def log(filename=None):
     def decorator(func):
         def wrapper(*args, **kwargs):
-            try:
-                start_time = time()
-                result = func(*args, **kwargs)
-                end_time = time()
-                if filename is None:
+            if filename is None:
+                try:
+                    start_time = time()
+                    result = func(*args, **kwargs)
+                    end_time = time()
                     print(f'{func.__name__} ok, start func: {start_time}, end func: {end_time}')
-                else:
-                    with open(filename, 'w') as file:
-                        file.write(f'{func.__name__} ok, start func: {start_time}, end func: {end_time}')
-            except type(error).__name__:
-                if filename is None:
+                    return result
+                except Exception as error:
                     print(f'{func.__name__} error: {type(error).__name__}. Inputs: {args}, {kwargs}')
-                else:
-                    with open('mylog.txt', 'w') as file:
-                        file.write(f'{func.__name__} error: {type(error).__name__}. Inputs: {args}, {kwargs}')
+            else:
+                try:
+                    start_time = time()
+                    result = func(*args, **kwargs)
+                    end_time = time()
+                    with open(filename, 'a') as file:
+                        file.write(f'{func.__name__} ok, start func: {start_time}, end func: {end_time}\n')
+                        return result
+                except Exception as error:
+                        with open(filename, 'a') as file:
+                            file.write(f'{func.__name__} error: {type(error).__name__}. Inputs: {args}, {kwargs}\n')
 
         return wrapper
     return decorator
---
+++ tests/test_decorators.py
@@ -0,0 +1,37 @@
+import pytest
+
+from src.decorators import log
+
+def test_log():
+    @log(filename="mylog.txt")
+    def my_function(x, y):
+        return x + y
+
+    my_function(1, 2)
+    with open("mylog.txt", "r") as file:
+        log_content = file.read()
+
+    assert 'my_function ok' in log_content
+
+
+
+
+def test_log_2(capsys):
+    @log()
+    def my_function(x, y):
+        return x + y
+
+    result = my_function(1, 2)
+    captured = capsys.readouterr()
+    assert 'my_function ok' in captured.out
+
+
+def test_log_3(capsys):
+    @log()
+    def my_function(x, y):
+        return x + y
+
+    result = my_function(1, 'a')
+    captured = capsys.readouterr()
+    assert 'my_function error' in captured.out
+
