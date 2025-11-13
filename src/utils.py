326880da27acdca1ef4bd12c31884f93e60e4c63
Sergey Mosykin <smosyakin@yandex.ru>
2025-11-13 14:35:13 +0300

    fixing utils.py and htmlcov

--- src/utils.py
+++ src/utils.py
@@ -6,13 +6,13 @@ def operations_data(route=None) -> list:
     if route is None:
         return []
     else:
-        with open(route, encoding="utf-8") as f:
-            try:
+        try:
+            with open(route, encoding="utf-8") as f:
                 content = f.read()
                 try:
                     data = json.loads(content)
                 except json.JSONDecodeError:
                     return []
-            except FileNotFoundError:
-                return []
-            return data
+        except FileNotFoundError:
+            return []
+        return data
